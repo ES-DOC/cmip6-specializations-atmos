58f8c2d3defade4857db83827210db6607713abe
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-05-24 20:26:06 +0200

    Standardising top-level realm module.

--- atmosphere.py
+++ atmosphere.py
@@ -1,6 +1,21 @@
+# --------------------------------------------------------------------
+# INTERNAL VARIABLES (do not change)
+# --------------------------------------------------------------------
+_TYPE = 'cim.2.science.scientific_realm'
+
+# --------------------------------------------------------------------
+# CONTACT
+#
+# Set to realm specialization co-ordinator.
+# --------------------------------------------------------------------
 CONTACT = 'Charlotte Pascoe'
 
-AUTHORS = ''
+# --------------------------------------------------------------------
+# CONTACT
+#
+# Set to realm specialization authors.
+# --------------------------------------------------------------------
+AUTHORS = 'Charlotte Pascoe'
 
 # --------------------------------------------------------------------
 # QUALITY CONTROL STATUS
@@ -12,16 +27,9 @@ QC_STATUS = 'draft'
 # --------------------------------------------------------------------
 # REALM IDENTIFIER
 #
-# Set to 'cmip6.<REALM>', e.g. 'cmip6.atmosphere'
+# Set to 'cmip6.<REALM>'
 # --------------------------------------------------------------------
 ID = 'cmip6.atmosphere'
-
-# --------------------------------------------------------------------
-# INTERNAL VARIABLES (do not change)
-# --------------------------------------------------------------------
-_TYPE = 'cim.2.science.scientific_realm'
-
-from collections import OrderedDict
 
 # --------------------------------------------------------------------
 # REALM: REALM
@@ -35,7 +43,7 @@ REALM = 'atmosphere'
 #
 # The grid used to layout the variables
 # --------------------------------------------------------------------
-GRID = ['atmosphere_grid']
+GRID = 'atmosphere_grid'
 
 # --------------------------------------------------------------------
 # REALM: KEY PROPERTIES
@@ -43,18 +51,19 @@ GRID = ['atmosphere_grid']
 # Key properties for the domain which differ from model defaults
 # (grid, timestep etc)
 # --------------------------------------------------------------------
-KEY_PROPERTIES = ['atmosphere_key_properties']
+KEY_PROPERTIES = 'atmosphere_key_properties'
 
 # --------------------------------------------------------------------
 # REALM: PROCESSES
 #
 # Processes simulated within the realm
 # --------------------------------------------------------------------
-PROCESSES = ['atmosphere_cloud_scheme',
-             'atmosphere_cloud_simulator',
-             'atmosphere_dynamical_core',
-             'atmosphere_gravity_waves',
-             'atmosphere_microphysics_precipitation',
-             'atmosphere_radiation',
-             'atmosphere_turbulence_convection',
-         ]
+PROCESSES = [
+	'atmosphere_cloud_scheme',
+    'atmosphere_cloud_simulator',
+    'atmosphere_dynamical_core',
+    'atmosphere_gravity_waves',
+    'atmosphere_microphysics_precipitation',
+    'atmosphere_radiation',
+    'atmosphere_turbulence_convection'
+    ]
